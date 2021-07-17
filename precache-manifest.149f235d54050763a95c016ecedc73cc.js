c43c02ee609cef270303c38b8afcefcf93d7e59d
Code Sandbox <hello@codesandbox.io>
2021-07-17 15:40:08 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.149f235d54050763a95c016ecedc73cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63b44c876f4724e735e9cd1ab9dea383",
+    "url": "https://kola9999.github.io/csb-93mxb/index.html"
+  },
+  {
+    "revision": "2abbe8eca89cd6244a0b",
+    "url": "https://kola9999.github.io/csb-93mxb/static/css/2.8ab860fe.chunk.css"
+  },
+  {
+    "revision": "b9890f9e2502d3c88d17",
+    "url": "https://kola9999.github.io/csb-93mxb/static/css/main.bbbbf122.chunk.css"
+  },
+  {
+    "revision": "2abbe8eca89cd6244a0b",
+    "url": "https://kola9999.github.io/csb-93mxb/static/js/2.ac62843f.chunk.js"
+  },
+  {
+    "revision": "731f815fa135396b018bfdb203439618",
+    "url": "https://kola9999.github.io/csb-93mxb/static/js/2.ac62843f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9890f9e2502d3c88d17",
+    "url": "https://kola9999.github.io/csb-93mxb/static/js/main.3af096a2.chunk.js"
+  },
+  {
+    "revision": "fd27c46f5b03f194161c",
+    "url": "https://kola9999.github.io/csb-93mxb/static/js/runtime-main.e704bd7a.js"
+  },
+  {
+    "revision": "813b6e7d5eec5840ff2929436c6a206c",
+    "url": "https://kola9999.github.io/csb-93mxb/static/media/404.813b6e7d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://kola9999.github.io/csb-93mxb/precache-manifest.149f235d54050763a95c016ecedc73cc.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://kola9999.github.io/csb-93mxb/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
